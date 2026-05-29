ee2f5778506481bc9b2afd0c7ee9ce864f582483
naitikdev8715 <naitikdiya1522@gmail.com>
2026-05-29 19:05:43 +0530

    Congratulations String Operations

---
+++ congratulationsstringoperations.py
@@ -0,0 +1,10 @@
+str1= "congratulations"
+print(str1.upper())
+print(str1.lower())
+print(str1[::-1])
+print(str1[5:12])
+print(str1[3:14])
+print(len(str1))
+str2= "congratu"
+str3= "lations"
+print(str2+str3)
