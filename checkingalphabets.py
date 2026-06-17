4dde345c14eb90d86a690d602da17f223fc9bac4
naitikdev8715 <naitikdiya1522@gmail.com>
2026-06-17 20:01:39 +0530

    Day 6 project

---
+++ checkingalphabets.py
@@ -0,0 +1,5 @@
+num= str(input("Please enter the character: "))
+if num== "a" or "b" or "c" or "d" or "e" or "f" or "g" or "h" or "i" or "j" or "k" or "l" or "m" or "n" or "o" or "p" or "q" or "r" or "s" or "t" or "u" or "v" or "w" or "x" or "y" or "z" :
+    print("It is an alphabet")
+else:
+    print("It is not an alphabet")
