6bf5122e5dea8e5050e2d5908236cc6227d33a0d
naitikdev8715 <naitikdiya1522@gmail.com>
2026-07-03 19:03:04 +0530

    Check age

---
+++ checkage.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age: "))
+if age < 10:
+    print("You can't enrol for the class")
+elif age == 10:
+    print("You can enrol for the class")
+elif age > 10:
+    print("You can enrol for the class")
+elif age == 20:
+        print("You can enrol for the class")
+elif age > 20:
+    print("You can't enrol for the class")
+else:
+    print("You can't enrol for the class")
